ea796824060b04a52a507732e9daa70c7e9c51cd
idhem1 <idhem.frelon@gmail.com>
2023-10-27 17:18:50 +0200

    a

--- projet.py
+++ projet.py
@@ -1,10 +1,24 @@
-import pyfirmata as pf
-import time
+import speech_recognition as sr
+import serial
 
-board = pf('COM5')
+ser = serial.Serial('COM9', 115200, timeout=0)
+r = sr.Recognizer()
 
 while True:
-    board.digital[2].write(1)
-    time.sleep(1)
-    board.digital[2].write(0)
-    time.sleep(1)
+    with sr.Microphone() as mic:
+        try:
+            print("Silence please, calibrating...")
+            r.adjust_for_ambient_noise(mic, duration=2)
+            print("calibrated, speak now...")
+            audio = r.listen(mic)
+            text = r.recognize_google(audio)
+            text = text.lower()
+            print("You said "+text+"\n")
+            ser.write(str.encode(text))
+            
+            
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+            
+        except sr.RequestError as e:
+            print("Request error; {0}".format(e))
