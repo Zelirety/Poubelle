59bac0356f3ae8856b22859a1dd2aefb9b5f9f7a
idhem <idhem.frelon@gmail.com>
2023-11-22 14:11:51 +0100

    a

---
+++ prog arduino/prog_arduino copy.ino
@@ -0,0 +1,33 @@
+String str;  
+
+void setup() {
+        Serial.begin(115200);      
+        pinMode(7, OUTPUT);
+}
+
+void loop() {
+        if (Serial.available() > 0) {
+                str = Serial.readStringUntil('\n');  
+             
+                if (str == "allumer led"){
+                  digitalWrite(7, HIGH);
+                }
+
+                 if (str == "éteindre led"){
+                  digitalWrite(7, LOW);
+
+                if (str == "ouvrir poubelle"){
+                        //OUVRIR
+                        //digitalWrite(7, HIGH)
+                        //delay(1000)
+                        //digitalWrite(7, LOW)
+
+                if (str == "fermer poubelle"){
+                        //OUVRIR
+                        //digitalWrite(7, LOW)
+                        //delay(1000)
+                        //digitalWrite(7, HIGH)
+                }
+                }
+        }
+}
