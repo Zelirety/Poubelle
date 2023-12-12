7fbaf94448dad4e870cf7f2f60de91fc9b7d2923
idhem <idhem.frelon@gmail.com>
2023-12-12 16:45:14 +0100

    a

--- projet.py
+++ projet.py
@@ -2,24 +2,27 @@ import speech_recognition as sr
 import serial
 
 ser = serial.Serial('COM4', 115200, timeout=0)
-r = sr.Recognizer()
+#r = sr.Recognizer()
 
 while True:
-    with sr.Microphone() as mic:
-        try:
-            print("silence, calibration...")
-            r.adjust_for_ambient_noise(mic, duration=1)
-            print("calibré, parlez")
-            audio = r.listen(mic)
-            text = r.recognize_google(audio, language='fr-FR')
-            text = text.lower()
-            print("Vous avez dit "+text+"\n")
-            ser.write(str.encode(text))
-            print(ser.readline())
+    #with sr.Microphone() as mic:
+    #    try:
+     #       print("silence, calibration...")
+      #      r.adjust_for_ambient_noise(mic, duration=1)
+       #     print("calibré, parlez")
+        #    audio = r.listen(mic)
+         #   text = r.recognize_google(audio, language='fr-FR')
+         #   text = text.lower()
+         #   print("Vous avez dit "+text+"\n")
+         #   ser.write(str.encode(text))
+         #   print(ser.readline())
             
             
-        except sr.UnknownValueError:
-            print("Could not understand audio")
+        #except sr.UnknownValueError:
+        #    print("Could not understand audio")
             
-        except sr.RequestError as e:
-            print("Request error; {0}".format(e))
+        #except sr.RequestError as e:
+        #    print("Request error; {0}".format(e))
+
+        if input('appuie : ') == 'ok':
+            ser.write('allumer moteur')
