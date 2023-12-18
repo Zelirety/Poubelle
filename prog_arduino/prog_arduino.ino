3ba30d6b52b8d5f57dff08980c84c3ff3941acc4
idhem1 <idhem.frelon@gmail.com>
2023-12-18 22:52:34 +0100

    a

--- prog_arduino/prog_arduino.ino
+++ prog_arduino/prog_arduino.ino
@@ -1,31 +1,39 @@
 #include "Ultrasonic.h"
-String str;  
+String str;
+int status;
 
 Ultrasonic ultrasonic(7);
 
 void setup() {
         Serial.begin(115200);      
         pinMode(8, OUTPUT);
+        status = 0; // Le couvercle est fermé
 }
 
 void loop() {
         if (Serial.available() > 0) {
                 str = Serial.readStringUntil('\n');  
              
-                if (str == "allumer moteur"){
+                if (str == "ouvrir poubelle" and status == 0){
                   digitalWrite(8, HIGH);
-                  //Serial.write()
-                  delay(50000);
+                  status = 1; //Le couvercle est ouvert
+                }
+
+                if (str == "fermer poubelle" and status == 1){
                   digitalWrite(8, LOW);
-                  
+                  delay(3000);
+                  status = 0; //Couvercle fermé
                 }
 
         }
-     long RangeInInches;
-     long RangeInCentimeters;
-    
-     RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
-     String range_to_string = String(RangeInCentimeters);
-     Serial.println(RangeInCentimeters);
-     delay(250);
+        if (status == 0){
+          long RangeInInches;
+          long RangeInCentimeters;
+          
+          RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // intervalles nécessaires entre 2 mesures
+          Serial.println(RangeInCentimeters);
+          delay(250);
+        }
+      
+     
 }
--- projet.py
+++ projet.py
@@ -1,34 +1,28 @@
-import speech_recognition as sr
-import serial
-import time
+import speech_recognition as sr #pip install SpeechRecognition et pip install pyaudio
+import serial #pip install pyserial
 
-ser = serial.Serial('COM3', 115200, timeout=0)
-#r = sr.Recognizer()
+ser = serial.Serial('COM4', 115200, timeout=0) #Mise en relation avec le port série (changer le 1er paramètre en fonction de l'ordinateur)
+r = sr.Recognizer() #Initialisation de SpeechRecognition
+profondeur_poubelle = 20 #Mesure en cm
 
 while (True):
-    #with sr.Microphone() as mic:
-    #    try:
-     #       print("silence, calibration...")
-      #      r.adjust_for_ambient_noise(mic, duration=1)
-       #     print("calibré, parlez")
-        #    audio = r.listen(mic)
-         #   text = r.recognize_google(audio, language='fr-FR')
-         #   text = text.lower()
-         #   print("Vous avez dit "+text+"\n")
-         #   ser.write(str.encode(text))
-         #   print(ser.readline())
+    with sr.Microphone() as mic:
+        try:
+            print("silence, calibration...")
+            r.adjust_for_ambient_noise(mic, duration=1)
+            print("calibré, parlez")
+            audio = r.listen(mic)
+            text = r.recognize_google(audio, language='fr-FR')
+            text = text.lower()
+            print("Vous avez dit "+text+"\n")
+            ser.write(str.encode(text))
             
+        except sr.UnknownValueError:
+            print("Could not understand audio")
             
-        #except sr.UnknownValueError:
-        #    print("Could not understand audio")
-            
-        #except sr.RequestError as e:
-        #    print("Request error; {0}".format(e))
-
-        #if input('appuie : ') == 'ok':
-            #ser.write(str.encode('allumer moteur'))
+        except sr.RequestError as e:
+            print("Request error; {0}".format(e))
 
         if (ser.in_waiting > 0):
-            print(ser.readline())
-        #time.sleep(0.26)
-        #.decode("utf-8")
+            ligne = str(ser.readline())[2:-5]
+            
